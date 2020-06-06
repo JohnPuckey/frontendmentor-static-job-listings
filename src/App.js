ce4033b2bdc0f031e01361acbf6954800159fa56
John <johnpuckeydesign@hotmail.com>
2020-06-06 19:01:35 +0100

    header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello World</h1>
     </div>
   );
