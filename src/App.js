01919aa11c7260e4763c91d6aa018b9baa5f3a53
John <johnpuckeydesign@hotmail.com>
2020-06-07 17:11:04 +0100

    mobile-first job-card styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Header from './components/Header';
+import JobCards from './components/JobCards'
+
+import jobData from './data/data.json';
+
+console.log(jobData[0])
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Hello World</h1>
+      {jobData.map((jobs) => {
+        return       <JobCards
+        key={jobs.id}
+        logo={jobs.logo}
+        company={jobs.company}
+        position={jobs.position}
+        postedAt={jobs.postedAt}
+        contract={jobs.contract}
+        location={jobs.location}
+        isFeatured={jobs.featured}
+        isNew={jobs.new}
+        role={jobs.role}
+        level={jobs.level}
+        languages={jobs.languages}
+        tools={jobs.tools}
+       />
+      })}
+
+  
     </div>
   );
 }
---
+++ src/components/JobCards.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const JobCards = ({
+    logo,
+    company,
+    position,
+    postedAt,
+    contract,
+    location,
+    isFeatured,
+    isNew,
+    role,
+    level,
+    languages,
+    tools
+}) => {
+
+    const jobTags = [role, level, ...(languages) || [], ...(tools) || []]
+
+    console.log(jobTags)
+
+    return (
+        <div className="job-card">
+            <div className="logo">
+                <img src={process.env.PUBLIC_URL + logo} alt={company}/>
+            </div>
+            <div className="card-details">
+                <h2 className="company">{company}</h2>
+
+                {/* check to see if job listing is featured or new */}
+                {isNew && <h2 className="new-tag">New!</h2>}
+                {isFeatured && <h2 className="feature-tag">Featured</h2>}
+                {isFeatured && <div className="feature-highlight"></div>}
+
+
+                <h1 className="position">{position}</h1>
+                <p className="description">{postedAt} <span>&#8226;</span> {contract} <span>&#8226;</span> {location}</p>
+            </div>
+
+            <div className="card-tags">
+                {jobTags.map((tag, index) => {
+                    return <button className="job-tag" key={index} tagname={tag}> {tag} </button>
+                })}
+            </div>
+
+        </div>
+    )
+}
+
+export default JobCards
