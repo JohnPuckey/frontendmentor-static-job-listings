c810bfd18e7769046a5ad239d47206fa0598204b
John <johnpuckeydesign@hotmail.com>
2020-06-08 20:33:49 +0100

    Updates

---
+++ precache-manifest.2676bb53f9435416fe070f236b1c6238.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f41a2ebbc4a0d67f57a20c71923cf8e",
+    "url": "/frontendmentor-static-job-listings/index.html"
+  },
+  {
+    "revision": "8aeffabfc09b88bddd3a",
+    "url": "/frontendmentor-static-job-listings/static/css/main.f90796c9.chunk.css"
+  },
+  {
+    "revision": "2b3d831caa511f777209",
+    "url": "/frontendmentor-static-job-listings/static/js/2.b9b9428a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontendmentor-static-job-listings/static/js/2.b9b9428a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aeffabfc09b88bddd3a",
+    "url": "/frontendmentor-static-job-listings/static/js/main.3721acbb.chunk.js"
+  },
+  {
+    "revision": "85b82081ab9d4599947e",
+    "url": "/frontendmentor-static-job-listings/static/js/runtime-main.15b5198c.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/frontendmentor-static-job-listings/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/frontendmentor-static-job-listings/static/media/bg-header-mobile.6b5ff5a0.svg"
+  }
+]);
