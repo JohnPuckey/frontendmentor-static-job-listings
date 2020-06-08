5884e5374346d8db9ddb01d2c40b1392c551f9ba
John <johnpuckeydesign@hotmail.com>
2020-06-08 20:33:10 +0100

    Small Adjustments, added attribution

--- src/App.js
+++ src/App.js
@@ -91,6 +91,10 @@ function App() {
       })}
       </div>
 
+      <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://www.frontendmentor.io/profile/ABCwarrior">John Puckey</a>.
+  </div>
   
     </div>
   );
